6ce00f218e2283499d0b39a289ccf3c2e9e0928f
zlatyprciny <zlaty.prciny@seznam.cz>
2020-12-06 03:04:04 +0100

    lazy module patients has been added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'patients', loadChildren: () => import('./patients/patients.module').then(m => m.PatientsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
